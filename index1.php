62093625ff7603abc27c85dd5a5ef26112fb6dc6
unknown <pap.ponce@yavirac.edu.ec>
2022-09-19 08:52:26 -0500

    chagra 2.4

--- index1.php
+++ index1.php
@@ -1,33 +1,197 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <link rel="stylesheet" href="sistema/css/styles.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <!-- CSS only -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  <link rel="stylesheet" href="sistema/css/styles8.css">
+  <link rel="stylesheet" href="sistema/css/fontawesome.min.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
+
 <body>
-<nav class="navbar navbar-expand-lg bg-danger">
-  <div class="container-fluid">
-        <ul></ul>
-      <span class="navbar-text">
-            0998085736
-      </span>
+  <nav class="navw navbar navbar-expand-lg bg">
+    <div class="container-fluid">
+      <ul>
+        <br>
+        <a href="https://www.facebook.com/ElchagraSupermercado/?ref=py_c" style="color: black; margin-left: 50px; "><i data-feather="facebook"></i></a>
+        <a style="color: black; margin-left: 30px; margin-top: 30px;"><i data-feather="instagram"></i></a>
+        <a style="color: black; margin-left: 30px;">
+          <ion-icon name="logo-tiktok"></ion-icon>
+        </a>
+      </ul>
+      <ul><br>
+        <img src="sistema/img/removebgchagra.png" width="180px" alt="" style="margin-right: 60px;">
+        <strong class="navbar-text text" style="margin-right: 6px;">
+          Contactanos:
+        </strong>
+        <span class="navbar-text" style="margin-right: 60px;">
+          <i data-feather="phone"></i> 0998085736
+        </span>
+      </ul>
     </div>
-  </div>
-</nav><br>
-<nav class="navbar navbar-expand-lg bg">
-  <div class="container-fluid">
-    <ul>
-        <img src="sistema/img/logoChagr.png" width="60px" alt="">   
-    </ul>   
     </div>
+  </nav>
+  <nav class="navbar bg-danger ali">
+    <div class="container-fluid">
+      <ul>
+        <p style="font-family: 'Dancing Script', cursive; margin-left: 57px;" class="colort fontz">Aquí si hay ahorro !!</p>
+      </ul>
+      <ul>
+        <form class="d-flex col-lg-11" role="search">
+          <input class="form-control me-2" style="width: 480px;" type="search" placeholder="Productos con descuentos siempre !!" aria-label="Search">
+          <button class="btn btn-outline-light"><i data-feather="search"></i></button>
+        </form>
+      </ul>
+      <ul>
+        <a href="sistema/quienes.php" class="btn colort">Quienes Somos</a>
+        <a href="sistema/index.php" class="btn colort">Iniciar Sesión</a>
+      </ul>
+    </div>
+  </nav>
+  <!---------------------------------------------------------------------------------------Carousel-------------------------------------------------------------------------------------->
+  <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="sistema/img/imgcar1.jpg" height="700" data-bs-interval="8000" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-md-block card col-md-4 mx-auto">
+          <h6 class="colorb"><span class="material-symbols-outlined colorb">liquor</span>Descuentos todos los dias<span class="material-symbols-outlined colorb">Nightlife</span></h6>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="sistema/img/imgcar2.jpg" height="700" data-bs-interval="2000" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-md-block card col-md-4 mx-auto">
+          <h6 class="colorb"><span class="material-symbols-outlined colorb">Paid</span>El 3x2 del Chagra<span class="material-symbols-outlined colorb">Paid</span></h6>
+          <p class="colorb">Solo en el Chagra lleva 3 productos de tu eleccion y paga 2 !</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="sistema/img/imgcar3.jpg" height="700" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-md-block card col-md-5 mx-auto">
+          <h6 class="colorb"><span class="material-symbols-outlined colorb">Diamond</span>Llevate algo adicional con La Yapita del Chagra<span class="material-symbols-outlined colorb">Diamond</span></h6>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div><br>
+  <div class="">
+    <h2 class="text-center colorb">OFERTAS DURANTE</h2>
+    <h2 class="text-center colorb">TODO EL AÑO</h2>
+    <div class="siz mx-auto colorb"></div><br><br>
+
   </div>
-</nav>
+  <!---------------------------------------------------------------------cards-------------------------------------------------------->
+  <div class="gray"><br><br><br>
+    <div class="container-fluid">
+      <div class="row">
+        <div style="margin-right: 2px; margin-left: 2px; margin-bottom: 30px;" class="bord bgr card col-5 mx-auto">
+          <div class="bgnone bord1 card-header">
+            <h3 class="colort text-center">Mascotas</h3>
+            <div class="siz mx-auto colory"></div>
+            <div class="card-body container-fluid">
+              <img class="" src="sistema/img/perrito.png" width="475" height="280">
+            </div>
+          </div>
+          <button class="boton cinco col-4 mx-auto">
+            <div class="icono">
+              <span class="material-symbols-outlined">
+                pets
+              </span>
+            </div>
+            <span>Ver mas</span>
+          </button><br>
+        </div>
+        <div style="margin-right: 2px; margin-left: 2px; margin-bottom: 30px;" class="bordbgr bg card col-5 mx-auto">
+          <div class="bgnone bord1 card-header">
+            <h3 class="colort text-center">Licores</h3>
+            <div class="siz mx-auto colorb"></div>
+            <div class="card-body container-fluid">
+              <img class="" src="sistema/img/licores.png" width="475" height="280">
+            </div>
+          </div>
+          <button class="boton2 cuatro col-4 mx-auto">
+            <div class="icono">
+              <span class="material-symbols-outlined">
+                local_bar
+              </span>
+            </div>
+            <span>Ver mas</span>
+          </button><br>
+        </div>
+      </div>
+      <div class="row">
+        <div style="margin-right: 2px; margin-left: 2px; margin-bottom: 30px;" class="bordbgr bg card col-5 mx-auto">
+          <div class="bgnone bord1 card-header">
+            <h3 class="colort text-center">Aseo Personal</h3>
+            <div class="siz mx-auto colorb"></div>
+            <div class="card-body container-fluid">
+              <img class="" src="sistema/img/aseo.png" width="475" height="310">
+            </div>
+          </div>
+          <button class="boton2 cuatro col-4 mx-auto">
+            <div class="icono">
+              <span class="material-symbols-outlined">
+                sanitizer
+              </span>
+            </div>
+            <span>Ver mas</span>
+          </button><br>
+        </div>
+        <div style="margin-right: 2px; margin-left: 2px; margin-bottom: 30px;" class="bord bgr card col-5 mx-auto">
+          <div class="bgnone bord1 card-header">
+            <h3 class="colort text-center">Condimentos</h3>
+            <div class="siz mx-auto colory"></div>
+            <div class="card-body container-fluid">
+              <img class="" src="sistema/img/condimensa.png" width="475" height="310">
+            </div>
+          </div>
+          <button class="boton cinco col-4 mx-auto">
+            <div class="icono">
+              <span class="material-symbols-outlined">
+                restaurant
+              </span>
+            </div>
+            <span>Ver mas</span>
+          </button><br>
+        </div>
+      </div>
+    </div><br><br>
+  </div><br>
+
+  <!----------------------------------------------------------------Beneficios------------------------------------------------------>
+  
+
 </body>
+<script src="https://unpkg.com/feather-icons"></script>
+<script>
+  feather.replace({
+    class: 'foo bar',
+    'stroke-width': 2,
+    'width': 18
+  })
+</script>
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+
 </html>
